7f297140217a52d8f54460da9e2a9aa6c91bd63c
付祖敏 <1326580471@qq.com>
2022-10-17 22:50:43 +0800

    初始化

--- vue3/4.放大镜/src/App.vue
+++ vue3/4.放大镜/src/App.vue
@@ -1,20 +1,16 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Magenifier from './components/Magenifier.vue'
+import lf from '@/assets/路飞头像1.png'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Magenifier
+    link="https://www.baidu.com"
+    blank
+    :imgUrl="lf"
+    imgWidth="350"
+    imgHeight="492"
+  ></Magenifier>
 </template>
 
 <style scoped>
---
+++ vue3/4.放大镜/src/components/Magenifier.vue
@@ -0,0 +1,84 @@
+<script lang="ts" setup>
+import { reactive, toRefs } from 'vue'
+
+type Props = {
+  link: string; // 跳转路径
+  blank: boolean; // 是否新窗口打开
+  imgUrl: string; // 图片地址
+  imgWidth: number; // 图片宽度
+  imgHeight: number; // 图片高度
+  magWidth: number; // 放大镜宽度
+  magHeight: number; // 放大镜高度
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  link: '',
+  blank: false,
+  imgUrl: '',
+  imgWidth: 100,
+  imgHeight: 100,
+  magWidth: 50,
+  magHeight: 50,
+})
+
+const move = () => {
+
+}
+
+const leave = () => {
+  
+}
+</script>
+
+<template>
+  <div class="img_wrap"
+    :style="{
+      width: imgWidth + 'px',
+      height: imgHeight + 'px',
+    }"
+    @mousemove="move"
+    @mouseleave="leave"
+  >
+    <a
+      :href="props.link"
+      class="mag_wrap"
+      :target="props.blank ? '_blank' : '_self'"
+      :style="{
+        width: props.magWidth + 'px',
+        height: props.magHeight + 'px',
+      }"
+    >
+      <img
+        :src="props.imgUrl"
+        alt="背景"
+        :style="{
+          width: props.imgWidth + 'px',
+          height: props.imgHeight + 'px',
+        }"
+      >
+    </a>
+
+    <a
+      :href="props.link"
+      class="img_lk"
+    >
+      <img
+        :src="props.imgUrl"
+        class="static_img"
+        alt="背景"
+      >
+    </a>
+    
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.img_wrap {
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
