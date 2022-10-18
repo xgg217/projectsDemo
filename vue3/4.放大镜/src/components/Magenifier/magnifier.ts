8de849dcb239bc5d669aa7f48ab8a4d835a8af0e
付祖敏 <1326580471@qq.com>
2022-10-18 22:53:37 +0800

    初始化

--- vue3/4.放大镜/src/components/Magenifier/magnifier.ts
+++ vue3/4.放大镜/src/components/Magenifier/magnifier.ts
@@ -2,9 +2,11 @@ export default {
   mounted: (el: Event) => {
     console.log(el);
     const oImgWrap = el as unknown as HTMLElement;
-    const OMagWrap = oImgWrap.querySelector('.mag_wrap')!;
+    const oMagWrap = (oImgWrap.querySelector('.mag_wrap')!) as unknown as HTMLElement;
     
-    const OMagImg = oImgWrap.querySelector('.mag_img')!;
+    const oMagImg = (oImgWrap.querySelector('.mag_img')!) as unknown as HTMLElement;
+    console.log(oMagImg);
+    
 
     // 图片宽高
     const oImgWrapStyle = getComputedStyle(oImgWrap, null);
@@ -13,9 +15,9 @@ export default {
     console.log(imgWidth, imgHeight);
 
     // 放大镜宽高
-    const OMagWrapStyle = getComputedStyle(OMagWrap, null);
-    const magWidth = parseInt(OMagWrapStyle.width);
-    const magHeight = parseInt(OMagWrapStyle.height);
+    const oMagWrapStyle = getComputedStyle(oMagWrap, null);
+    const magWidth = parseInt(oMagWrapStyle.width);
+    const magHeight = parseInt(oMagWrapStyle.height);
     console.log(magWidth, magHeight);
     
 
@@ -28,18 +30,43 @@ export default {
 
     const bindEvent = () => {
       oImgWrap.addEventListener('mouseover', (e) => {
-        OMagWrap.classList.add('show');
+        oMagWrap.classList.add('show');
+
+        // 开启监听
         document.addEventListener('mousemove', handleMouseMove, false);
       }, false);
 
       oImgWrap.addEventListener('mouseout', handleMouseOut, false);
     }
 
-    const handleMouseMove = (e: MouseEvent) => {}
+    const handleMouseMove = (e: MouseEvent) => {
+
+    }
 
     const handleMouseOut = (e: MouseEvent) => {
-      OMagWrap.classList.remove('show');
+      oMagWrap.classList.remove('show');
+      // 取消监听
+      document.removeEventListener('mousemove', handleMouseMove, false);
     }
+
+    const getXY = (e: MouseEvent) => {
+      return {
+        x: e.pageX - imgX - magWidth / 2, // 鼠标相对于图片的位置
+        y: e.pageY - imgY - magHeight / 2,
+        mouseX: e.pageX - imgX, // 边界
+        mouseY: e.pageY - imgY,
+      }
+    }
+
+    const showMag = (x:number,y:number,mouseX:number,mouseY:number) => {
+      oImgWrap.style.left = `${x}px`;
+      oImgWrap.style.top = `${y}px`;
+
+      oMagImg.style.left = `${-x}px`;
+      oMagImg.style.top = `${-y}px`;
+    }
+
+
 
     const init = () => {
       bindEvent();
