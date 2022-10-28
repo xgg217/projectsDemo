b26aea3caa764992210a9d6f5285d2602a114c0b
付祖敏 <1326580471@qq.com>
2022-10-28 18:04:11 +0800

    日历初始化

---
+++ vue3/FightingDesign/src/components/calendar/calendar.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>日历</div>
+</template>
+
+<style lang="scss" scoped></style>
--- vue3/FightingDesign/src/views/calendar/index.vue
+++ vue3/FightingDesign/src/views/calendar/index.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
+import WCalendar from "@/components/calendar/calendar.vue";
 const date = new Date();
 </script>
 
 <template>
-  <div>
+  <div class="warpp">
     <f-calendar v-model:date="date" />
+    <WCalendar />
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.warpp {
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid red;
+}
+</style>
