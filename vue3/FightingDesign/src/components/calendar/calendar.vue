43c4a47af22cbeeaddbe2aef70558e4ff2255209
付祖敏 <1326580471@qq.com>
2022-10-28 18:19:08 +0800

    初始化

--- vue3/FightingDesign/src/components/calendar/calendar.vue
+++ vue3/FightingDesign/src/components/calendar/calendar.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Props, Emits } from "./calendar";
+</script>
 
 <template>
-  <div>日历</div>
+  <div>
+    <header>
+      <div>
+        <span>当前时间</span>
+      </div>
+
+      <div>按钮</div>
+    </header>
+    日历
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
